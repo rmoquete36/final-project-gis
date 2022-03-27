632484840952fb1c883438d08a4b98d32389e531
rmoquete36 <98977363+rmoquete36@users.noreply.github.com>
2022-03-27 17:11:55 -0400

    Text modifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-
   mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmYTQ2YzBmMTM0ZDAyIn0.owNd_Qa7Sw2neNJbK6zc1A'
 
   // lngLat to show entire NYC on load
@@ -27,31 +26,30 @@
     source: 'nypdpp-scounts',
     paint: {
       'fill-color': [
-        'interpolate',
-        ['linear'],
-        ['get', 'count_vals'],
-        0,
-        '#fff7fb',
-        10,
-        '#ece7f2',
-        20,
-        '#d0d1e6',
-        30,
-        '#a6bddb',
-        40,
-        '#74a9cf',
-        50,
-        '#3690c0',
-        60,
-        '#0570b0',
-        70,
-        '#045a8d',
-        80,
-        '#023858'
-      ],
+            'interpolate',
+            ['linear'],
+            ['get', 'count_vals'],
+            0,
+            '#fff7fb',
+            10,
+            '#ece7f2',
+            20,
+            '#d0d1e6',
+            30,
+            '#a6bddb',
+            40,
+            '#74a9cf',
+            50,
+            '#3690c0',
+            60,
+            '#0570b0',
+            70,
+            '#045a8d',
+            80,
+            '#023858'
+          ],
     'fill-outline-color': '#171515'
-    // alternate outline color: #6a7280
-    }
+      }
     });
 
   map.addSource('census-08-12', {
@@ -65,7 +63,7 @@
     type: 'fill',
     source: 'census-08-12',
     paint: {
-    'fill-opacity': .95,
+    'fill-opacity': .7,
     'fill-outline-color': '#f5eeed',
     'fill-color': '#8994a1'
   }
@@ -109,28 +107,10 @@
       <li><strong>Total Population:</strong> ${e.features[0].properties.poptot}</li>
       <li><strong>Population Unemployed:</strong> ${e.features[0].properties.popunemplo}</li>
       <li><strong>Population Poor/Struggling:</strong> ${e.features[0].properties.poororstru}</li>
-      <li><strong>Residents on Public Assistance:</strong> ${e.features[0].properties.withpubass}</li>
-      `)
-    // .setHTML(popupContent)
+      <li><strong>Families on Public Assistance:</strong> ${e.features[0].properties.withpubass}</li>
+        `)
     .addTo(map);
-  // popup.setLngLat(coordinates).setHTML(popupContent).addTo(map);
    });
-
-
-// pull these properties from data for popup
-    // const coordinates = e.features[0].geometry.coordinates.slice();
-    // const neighborhood = e.features[0].properties.ntaname;
-    // const population = e.features[0].properties.poptot;
-    // const unemployed = e.features[0].properties.popunemplo;
-    // const poor = e.features[0].properties.poororstru;
-    //
-    // var popupContent = `
-    //   <h3>${neighborhood}</h3>
-    //   <ul>
-    //     <li><strong>Unemployed:</strong> ${unemployed}</li>
-    //     <li><strong>Poor or Struggling:</strong> ${poor}</li>
-    //     <li><strong>Total Population:</strong> ${population}</li>
-    // `
 
 
   // listen for click on the 'Back to City View' button
@@ -139,76 +119,11 @@
     })
 
 
-
-
-
-
-    //
-    // $('#toggle-census-data').on('click', function() {
-    //   var visibility = map.getLayoutProperty('census-08-12-line', 'visibility')
-    //   if (visibility === 'none') {
-    //     map.setLayoutProperty('census-08-12-line', 'visibility', 'visible');
-    //   } else {
-    //     map.setLayoutProperty('census-08-12-line', 'visibility', 'none');
-    //   }
-    // })
-    // })
-
-
-
-  //   // When a click event occurs on a feature in the census-08-12,
-  //   // open a popup at the location of the click, with nta name
-  //   // HTML from the click event's properties.
-  //   map.on('click', 'nypdpp-scounts-fill', function(e) {
-  //   new mapboxgl.Popup()
-  //   // .setLngLat(e.coordinates)
-  //   .setHTML(e.features[0].properties.poptot)
-  //   .addTo(map);
-  // });
-  //
-  //
-  //   // Create a popup.
-  //   const popup = new mapboxgl.Popup({
-  //     closeButton: false,
-  //     closeOnClick: false
-  //   });
-  //
-  //   map.on('mouseenter', 'robberymap-circle', function(e) {
-  //     // Change the cursor style as a UI indicator.
-  //     map.getCanvas().style.cursor = 'pointer';
-  //
-  //     // Copy coordinates array.
-  //     const coordinates = e.features[0].geometry.coordinates.slice();
-  //     const robType = e.features[0].properties.PD_DESC;
-  //     const precinct = e.features[0].properties.ADDR_PCT_CD;
-  //
-  //     // Ensure that if the map is zoomed out such that multiple
-  //     // copies of the feature are visible, the popup appears
-  //     // over the copy being pointed to.
-  //     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-  //     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-  //     }
-  //
-  //   // Populate the popup and set its coordinates
-  //   // based on the feature found.
-  //       popup.setLngLat(coordinates).setHTML(`${precinct}`).addTo(map);
-  //   });
-  //
-  //       map.on('mouseleave', 'robberymap-circle', function() {
-  //       map.getCanvas().style.cursor = '';
-  //       popup.remove();
-  //     });
-
-  // style these lines differently - MAY NOT NEED THIS PIECE OF CODE
-    // map.addLayer({
-    //   id: 'census-08-12-line',
-    //   type: 'line',
-    //   source: 'census-08-12',
-    //   paint: {
-    //   'line-color': '#1f1d1d'
-    // }
-    // });
-
-
+      // Ensure that if the map is zoomed out such that multiple
+      // copies of the feature are visible, the popup appears
+      // over the copy being pointed to.
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
 
 });
